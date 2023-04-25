bf802415fe32e6fe987362cc1cee6e6f6ca3f54d
Benedikt Galbavy <if22b082@technikum-wien.at>
2023-04-25 19:31:40 +0200

    basic appointment form

--- common.js
+++ common.js
@@ -1,5 +1,6 @@
 $("#sendLogin").on("click", login);
 $("#sendSignup").on("click", signup);
+$("#sendAppoint").on("click", newAppoint);
 
 function login() {
 	var form_data = { };
@@ -35,4 +36,39 @@ function signup() {
 	    	$('#user-button').html(response);
 	    }        
 	});
+}
+
+function newAppoint() {
+	var form_data = { };
+	var timeslots = [ ];
+	$.each($('#signup').serializeArray(), function() {
+		if (this.name.startsWith("start")) {
+			timeslots.push({
+				start: this.value,
+			});
+		} else if (this.name.startsWith("end")) {
+			timeslots[this.name.splice(3)] = {
+				end: this.value,
+			};
+		} else {
+    		form_data[this.name] = this.value;
+		}
+	});
+
+	form_data['timeslots'] = timeslots;
+
+	$.ajax({
+	    type: "GET",
+	    url: "../service_handler.php",
+	    cache: false,
+	    data: { method: "signup", param: form_data },
+	    dataType: "json",
+	    success: function (response) {
+	    	$('#user-button').html(response);
+	    }        
+	});
+}
+
+function newTimeslotField() {
+	// TODO
 }
